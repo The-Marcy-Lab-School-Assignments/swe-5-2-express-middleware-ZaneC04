c0ca274092e84e11d17b2c03ee9e27f61a4af87e
ZaneC04 <zenithzsc@gmail.com>
2026-03-04 17:08:44 -0500

    wrote all index.js code

--- server/index.js
+++ server/index.js
@@ -22,11 +22,23 @@ const quotes = [
 
 // 1. logRoutes — logs the HTTP method, URL, and timestamp for every request, then calls next()
 
+
+const logRoutes = (req, res, next) => {
+  const time = new Date().toLocaleString();
+  console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+  next(); 
+};
+
+
 // 2. express.static() — generates middleware that serves files from the frontend/ folder
 //    Use path.join(__dirname, '../frontend') to construct the absolute path
 
-// TODO: Register middleware with app.use() before the controllers
+const pathToFrontend = path.join(__dirname, '../frontend');
+const serveStatic = express.static(pathToFrontend);
+app.use(serveStatic);
 
+// TODO: Register middleware with app.use() before the controllers
+app.use(logRoutes);
 
 
 // TODO: Define controllers here
@@ -34,22 +46,45 @@ const quotes = [
 // listQuotes — sends all quotes as JSON
 //   If the request includes a ?topic= query string, send only quotes with a matching topic
 
+const listQuotes = (req, res, next) => {
+  const contains = req.query.topic
+  if (contains) {
+    const filteredQuotes = quotes.filter(quote => quote.topic === contains.toLowerCase())
+    res.send(filteredQuotes)
+  } else {
+    res.send(quotes)
+  }
+  
+}
+
+
 // getQuote — sends a single quote whose id matches req.params.id
 //   If no matching quote is found, respond with 404 and { error: 'No quote with id <id>' }
 
-
+const getQuote = (req, res, next) => {
+  const { id } = req.params
+  const filteredQuote = quotes.find(quote => quote.id === Number(id))
+  if (!filteredQuote) {
+    res.status(404).send({ error: `No quote with id ${id}` });
+  }
+  res.send(filteredQuote)
+}
 
 // TODO: Register endpoints here
 
 // GET /api/quotes
 // GET /api/quotes/:id
-
-
+app.get('/api/quotes/', listQuotes)
+app.get("/api/quotes/:id", getQuote)
 
 // TODO: Add a catch-all fallback that responds with 404 and { error: 'Not found: <url>' }
 // Use app.use() and place it after all other routes
 
+const serve404 = (req, res, next) => {
+  res.status(404).send({ error: `Not found: ${req.originalUrl}` });
+}
 
+app.use(serve404)
 
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
